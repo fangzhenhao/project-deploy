27cd71d9774a1b6cdd80403390e4ffac31d29429
ttls <fangzhenhao@live.com>
2024-12-01 22:59:02 +0800

    initialize

---
+++ src/main/java/com/skytech/projectdeploy/ProjectDeployApplication.java
@@ -0,0 +1,13 @@
+package com.skytech.projectdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectDeployApplication.class, args);
+    }
+
+}
